3bc5c70b80c5ad41019e93ca392774037e78713a
Rob Ranf <rob@emiyaconsulting.com>
2025-05-16 20:41:53 -0700

    detail views

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -34,8 +34,11 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Resorts")
+            .navigationDestination(for: Resort.self) { resort in
+                ResortView(resort: resort)
+            }
         } detail: {
-            Text("Detail")
+            WelcomeView()
         }
     }
 }
